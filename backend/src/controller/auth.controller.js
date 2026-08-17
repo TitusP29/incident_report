a9dd1522a5c832a7d43bf8cc0ec4baf04d394ea6
Titus Kubayi <tituskubayi5@gmail.com>
2026-08-17 04:19:45 +0200

    Login and Logout functionality added

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -41,8 +41,8 @@ export const signup = async (req, res) => {
 
           //after CRUD
           //Persist user first, then issue auth cookie
-        const savedUserb = await newUser.save();
-        generateToken(savedUserb._id, res);
+        const savedUser = await newUser.save();
+        generateToken(savedUser._id, res);
 
           res.status(201).json({
             _id: newUser._id,
@@ -64,4 +64,33 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller:", error)
         res.status(500).json({ message: "Internal server error" });
     }
+};
+
+export const login = async (req, res) => {
+  const { email, password} = req.body
+
+  try{
+    const user = await User.findOne({email})
+    if(!user) return res.status(400).json({message:"Invalid Credentials"})
+      // never tell the client which one is incorrent: password or email
+
+    const isPasswordCorrect = await bcrypt.compare(password,user.password)
+    if(!isPasswordCorrect) return res.status(400).json({message: "Invalid Credentials"});
+
+    generateToken(user._id,res)
+
+    res.status(200).json({
+      _id: user.id,
+      fullName: user.fullName,
+      image: user.image,
+    });
+  } catch (error){
+    console.error("Error in login controller:",error)
+    res.status(500).json({messaged:"Internal server error"})
+  }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt","", {maxAge: 0});
+  res.status(200).json({ message: "Logged out successfully" });
 };
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,10 +6,10 @@ export const connectDB =  async () => {
     const { MONGO_URI } = ENV;
     if (!MONGO_URI) throw new Error("MONGO_URI is not set");
     
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(ENV.MONGO_URI)
     console.log("MONGODB CONNECTED", conn.connection.host)
 } catch (error) {
     console.error("Error connection to MONGODB:", error)
     process.exit(1); // 1 status code means fail, 0 means success
 }
-}
+};
